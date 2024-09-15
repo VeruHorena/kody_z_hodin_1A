9cf5c273db69355f3600543bae7b93cae9baf527
VeruHorena <136056794+VeruHorena@users.noreply.github.com>
2024-09-15 10:22:17 +0200

    Delete 01-Uvod-010-Pozdrav/01-Uvod-010-Pozdrav/01-Uvod-010-Pozdrav directory

--- 01-Uvod-010-Pozdrav/01-Uvod-010-Pozdrav/01-Uvod-010-Pozdrav/.lesson/instructions.md
+++
@@ -1,5 +0,0 @@
-# Pozdrav
-
-Tento program by se měl uživatele zeptat na jméno a pak jej pozdravit. 
-
-Z nějakého důvodu ale nechodí. Dokážete jej opravit?
--- 01-Uvod-010-Pozdrav/01-Uvod-010-Pozdrav/01-Uvod-010-Pozdrav/main.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class MainClass {
-    public static void Main (string[] args) {
-        Console.WriteLine ("Vlož své jméno :");
-       string jmeno = Console.ReadLine();
-        Console.WriteLine ("Ať žije " + jmeno + "!");
-    }
-}
