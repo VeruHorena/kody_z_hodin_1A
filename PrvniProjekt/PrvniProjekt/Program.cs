cbd5aed92595feda96625f82bbb8703fb9e4cff3
VeruHorena <horenovska.veronika@gmail.com>
2024-09-27 11:24:06 +0200

    kod 27_9

---
+++ PrvniProjekt/PrvniProjekt/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PrvniProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Ahoj uzivateli");
+            int a;
+            a = 18;
+            Console.WriteLine("Prirazena hodnota a je "+a);
+            */
+            // Console.ReadKey(); //pomoci toho se mi nezavre konzole a ceka na stisknuti libovolneho znaku
+
+
+
+            /*Console.WriteLine("Ahoj uzivateli, jsem papousek Lora");
+            Console.Write("Zadej mi text k opakovani: ");
+            string text;
+            text = Console.ReadLine();
+            Console.WriteLine(text + " " + text);
+            Console.ReadKey();
+        */
+
+            Console.WriteLine("Zadej hodnotu k vynasobeni: ");
+            int cislo; //deklarace
+           
+            cislo = int.Parse(Console.ReadLine());
+            int vysledek = cislo * 2; //definice
+            Console.WriteLine("Vysledek je: " + vysledek);
+        }
+    }
+}
