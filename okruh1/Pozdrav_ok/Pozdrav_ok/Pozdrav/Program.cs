cae9cf461ae630043308c2be57d76f7770acdddd
VeruHorena <horenovska.veronika@gmail.com>
2024-09-30 11:43:21 +0200

    kod 30_9

---
+++ okruh1/Pozdrav_ok/Pozdrav_ok/Pozdrav/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Vlož své jméno :");
+        int jmeno = Console.ReadLine();
+        Console.WriteLine("Ať žije " + jmeno + "!");
+    }
+}
---
+++ okruh1/kalkulator_ok/kalkulator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Zadej prvni cislo: ");
+        int a = int.Parse(Console.ReadLine()); // nacteni prvni hodnoty
+        /*viceradkovy komentar */
+        Console.WriteLine("Zadej druhe cislo: ");
+        int b = int.Parse(Console.ReadLine());
+        int vysledek = a + b;
+        Console.WriteLine("Soucet cisel je: " + vysledek);
+        vysledek = a - b; //prepisu obsah promenne novou hodnotou
+        Console.WriteLine("Rozdil cisel je: " + vysledek);
+        vysledek = a * b;
+        Console.WriteLine("Soucin cisel je: " + vysledek);
+        float vysledek2;
+        vysledek2 = (float)a / b;
+        Console.WriteLine("Podil cisel je: " + vysledek2);
+    }
+}
