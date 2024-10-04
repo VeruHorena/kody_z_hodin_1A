7c60f384252bdfcfdd3bab5c88629dec1061a3f3
VeruHorena <horenovska.veronika@gmail.com>
2024-10-04 11:24:12 +0200

    podminky, hodina 4_10

---
+++ hodina4_10/hodina4_10/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace hodina4_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Console.WriteLine("Zadej hodnotu:");
+              int hodnota = int.Parse(Console.ReadLine());
+
+              if (hodnota >= 10)
+              {
+                  //vetev true
+                  Console.WriteLine("Hodnota je vetsi nez 10.");
+              }
+              else
+              {
+                  //vetev false
+                  Console.WriteLine("Hodnota neni vetsi nez 10.");
+              }
+            */
+            Console.WriteLine("Zadej text:");
+            string text = Console.ReadLine();
+            
+            if (text == "Ahoj" || text=="ahoj")
+            {
+                Console.WriteLine("Text byl ahoj");
+            }
+            else
+            {
+                Console.WriteLine("Text byl cau");
+            }
+        }
+    }
+}
