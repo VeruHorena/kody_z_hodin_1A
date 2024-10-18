8110b5c8f0d4fa33996c1691a0515cf58de041f1
VeruHorena <horenovska.veronika@gmail.com>
2024-10-18 12:09:19 +0200

    nahodna_cisla

---
+++ nah_cisla/nah_cisla/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace nah_cisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Random generator = new Random(); //vytvoreni objektu pro generovani cisel
+int nahodne_cislo = generator.Next(); //bez rozsahu nahodne cele cislo v rozsahu int
+            Console.WriteLine("Bez rozsahu nahodne cislo: " + nahodne_cislo);
+            int nahodne2 = generator.Next(1, 11); //generovani cisla v rozsahu 1-10
+            Console.WriteLine("Rozsah 1-10 nahodne cislo: " + nahodne2);
+            double nahodneDes = generator.NextDouble(); // generovani des. cisla v rozsahu <0.0,1.0)
+            Console.WriteLine("Desetinne nahodne cislo: " + nahodneDes);
+        }
+    }
+}
