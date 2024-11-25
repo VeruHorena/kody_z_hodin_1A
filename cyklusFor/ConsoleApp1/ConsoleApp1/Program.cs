76b65ecaac02064a21bf12c21cd27df33e7d3a11
VeruHorena <horenovska.veronika@gmail.com>
2024-11-25 10:46:53 +0100

    Cyklus for + tvary

---
+++ cyklusFor/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //vypsani cisel 1-10
+          /* for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            //vypsani cisel 10-1
+            /*for(int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            //vypsani sudych cisel
+            for(int i = 2; i <= 10; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ kresleniTvaru/kresleni_tvary/kresleni_tvary/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace kresleni_tvary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kresleni obdelnik
+            Console.Write("Zadejte vysku obdelniku: ");
+            int vyska = int.Parse(Console.ReadLine());
+
+            Console.Write("Zadejte sirku obdelniku:  ");
+            int sirka = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            // Vykresleni
+            for (int i = 0; i < vyska; i++) // radky
+            {
+                for (int j = 0; j < sirka; j++) // sloupce
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); //novy radek
+            }
+
+            //rovnoramenny trojuhelnik
+            Console.Write("Zadejte výšku trojúhelníku: ");
+            int vyskatrojuhelnik = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            // Vykreslení trojúhelníku
+            for (int i = 0; i < vyska; i++) // Procházíme řádky
+            {
+                // Vykreslení mezer před hvězdičkami
+                for (int j = 0; j < vyska - i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Vykreslení hvězdiček
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine(); // Přechod na nový řádek
+            }
+
+
+            //trojuhelnik typu p
+            Console.Write("Zadejte délku strany pro trojuhelnik: ");
+            int delka = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = delka; i > 0; i--) // start na nejsirsim radku
+            {
+                for (int j = 0; j < i; j++) //vykreslime hvezdicky odpovidajici sirce radku
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); // novy radek
+            }
+        }
+    }
+}
