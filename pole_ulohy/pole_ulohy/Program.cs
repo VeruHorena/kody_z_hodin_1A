775f98baf338219439ec6615344422864feea42d
VeruHorena <horenovska.veronika@gmail.com>
2024-12-02 10:40:53 +0100

    pole

---
+++ pole_ulohy/pole_ulohy/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace pole_ulohy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int velikost;
+            do
+            {
+                Console.WriteLine("Zadej velikost pole:");
+            }while(int.TryParse(Console.ReadLine(), out velikost)==false || velikost<=0);
+            int[] pole = new int[velikost];
+
+            for (int i = 0; i < pole.Length; i++)
+            {
+                do
+                {
+                    Console.WriteLine("Zadej hodnotu:");
+                }while(int.TryParse(Console.ReadLine(),out pole[i]) == false);
+            }
+
+            Console.WriteLine("Vypsane pole");
+            for (int i = 0; i < pole.Length; i++)
+            {
+                Console.Write($"{pole[i]} ");
+            }
+            Console.WriteLine();
+           Console.WriteLine("Vypsane pole pomoci foreach");
+            foreach(int vcela in pole)
+            { 
+                Console.Write($"{vcela} ");
+            }
+        }
+    }
+}
