cebb5ace853f8dc5f02740c531c0703c7808c772
VeruHorena <horenovska.veronika@gmail.com>
2024-12-06 11:15:01 +0100

    Fibonacciho rada

---
+++ FiboPole/FiboPole/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FiboPole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            do
+            {
+                Console.WriteLine("Zadej pocet prvku rady:");
+            } while (int.TryParse(Console.ReadLine(), out n)==false || n <= 0);
+
+            int[] rada = new int[n];
+            //pocatecni 1 Fibonacciho rady
+            rada[0] = 1;
+            rada[1] = 1;
+            for (int i = 2; i < n; i++)
+            {
+                rada[i] = rada[i - 1] + rada[i - 2];
+            }
+
+            Console.WriteLine("Vypis Fibonacciho rady");
+            foreach (int prvek in rada)
+            {
+                Console.Write($"{prvek} ");
+            }
+        }
+    }
+}
