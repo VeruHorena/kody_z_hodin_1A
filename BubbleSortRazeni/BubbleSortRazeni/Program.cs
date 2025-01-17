d0c305d1087f3aaff3c538561e58eb869d25d3fa
VeruHorena <horenovska.veronika@gmail.com>
2025-01-17 10:50:04 +0100

    metody

---
+++ BubbleSortRazeni/BubbleSortRazeni/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace BubbleSortRazeni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] poleHodnot = new int[1000];
+            int pom;
+            Random generator = new Random();
+            Stopwatch stopky = new Stopwatch(); //jeste nejsou spustene
+
+            for (int i = 0; i < poleHodnot.Length; i++)
+            {
+                poleHodnot[i] = generator.Next(1,101);
+               // Console.Write(poleHodnot[i] + ",");
+            }
+
+            stopky.Start(); //zacneme merit cas
+            for(int i = 0; i < poleHodnot.Length - 1; i++)
+            {
+                for(int j = 0;j<poleHodnot.Length -1 - i; j++)
+                {
+                    if (poleHodnot[j] > poleHodnot[j + 1])
+                    {
+                        pom = poleHodnot[j];
+                        poleHodnot[j] = poleHodnot[j + 1];
+                        poleHodnot[j + 1] = pom;
+                    }
+                }
+            }
+            stopky.Stop();
+            long casBubble = stopky.ElapsedMilliseconds;
+            for (int i = 0;i<poleHodnot.Length;i++)
+            {
+                Console.Write(poleHodnot[i] + ",");
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("BubbleSort " + casBubble + " ms");
+            
+
+
+        }
+    }
+}
---
+++ metodyPozdrav/metodyPozdrav/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace metodyPozdrav
+{
+    internal class Program
+    {
+        public static void Pozdrav(String jmenoUzivatele, int pocetPrihlaseni = 1)
+        {
+            int prihlaseni = Soucet(pocetPrihlaseni, 1);
+            Console.WriteLine("Ahoj uzivateli " + jmenoUzivatele + "! jsi tu po: " + prihlaseni);
+
+        }
+
+        public static int Soucet (int a, int b)
+        {
+            Console.WriteLine("Jsem v metode soucet.");
+            int soucet = a + b;
+            return soucet;
+        }
+
+        public static int[] vratLiche(int[] poleCisel)
+        {
+            int[] vysledek = new int[poleCisel.Length];
+            int pozice = 0;
+            for (int i = 0; i < poleCisel.Length; i++)
+            {
+                if (poleCisel[i] % 2 != 0)
+                {
+                    vysledek[pozice] = poleCisel[i];
+                    pozice++;
+                }
+            }
+            return vysledek;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            string uzivatel = "PepaVyskoc";
+            Pozdrav(uzivatel,6);
+           int v =  Soucet(5, 3);
+            Console.WriteLine("Vysledek souctu je: " + v);
+            Console.WriteLine("Vysledek souctu 1 + 1 je: " + Soucet(1,1));
+
+            int[] zadani = new int[10];
+            Random generator = new Random();
+            for(int i = 0;i < zadani.Length; i++)
+            {
+                zadani[i] = generator.Next(1,11);
+            }
+
+            int[] lichaCisla = vratLiche(zadani);
+            foreach(int i in lichaCisla)
+            {
+                if(i != 0)
+                Console.Write(i + ", ");
+            }
+        }
+    }
+}
