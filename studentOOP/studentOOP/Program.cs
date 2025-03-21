f2deb3e28823f63aec8ef327b68a4cffea3b5dd7
VeruHorena <horenovska.veronika@gmail.com>
2025-03-21 11:24:04 +0100

    oop1

---
+++ studentOOP/studentOOP/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace studentOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student();
+            st1.PredstavSe();
+            Student st2 = new Student("Pepa", 5, "Slunickova 3");
+            Console.WriteLine();
+            st2.setZnamka(2);
+            st2.setZnamka(1);
+            foreach (int znamka in st2.getZnamky())
+            {
+                Console.Write(znamka + ", ");
+            }
+            Console.WriteLine();
+            st2.PredstavSe();
+            st1.setJmeno("Johana");
+            st1.setVek(6);
+            st1.setAdresa("S dlouhyma nohama");
+            st1.PredstavSe();
+        }
+    }
+}
---
+++ studentOOP/studentOOP/Student.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentOOP
+{
+    class Student
+    {
+        private String jmeno;
+        private int vek;
+        private String adresa;
+        private List<int> seznamZnamek;
+
+        public void setJmeno (String jmeno)
+        {
+            this.jmeno = jmeno;
+        }
+
+        public String getJmeno()
+        {
+            return this.jmeno;
+        }
+
+        public void setVek (int vek)
+        {
+            this.vek = vek;
+        }
+        public int getVek()
+        {
+            return this.vek;
+        }
+
+        public void setAdresa (String adresa)
+        {
+            this.adresa = adresa;
+        }
+
+        public String getAdresa()
+        {
+            return this.adresa;
+        }
+
+        public void setZnamka(int znamka)
+        {
+            this.seznamZnamek.Add(znamka);  
+        }
+
+        public List<int> getZnamky()
+        {
+            return this.seznamZnamek;
+        }
+        public Student()
+        {
+            this.setJmeno("prazdne");
+            this.setVek(0);
+            this.setAdresa("prazdne");
+            this.seznamZnamek = new List<int>();
+        }
+
+        public Student (string jmeno, int vek, string adresa)
+        {
+            this.setJmeno(jmeno);
+            this.setVek(vek);
+            this.setAdresa(adresa);
+            this.seznamZnamek = new List<int>();
+        }
+
+        public void PredstavSe()
+        {
+
+            Console.WriteLine($"Ahoj jsem {this.getJmeno()}, " +
+                $"je mi {this.getVek()} let a bydlim na {this.getAdresa()}");
+        }
+    }
+}
